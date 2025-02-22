52974e93bf0783d5f187fc47d976dc1adde66ddd
ABE HIRONAO <hna@HNAs-MBP-2023.local>
2025-02-22 15:38:50 +0900

    feat: add button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <h1>Hello Next App.</h1>
+    <div>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        すべての人間は
+      </h1>
+      <Button variant="outline" size="sm" disabled>
+        Button
+      </Button>
+      <Button disabled>
+        <Link href="/login">Login</Link>
+      </Button>
     </div>
   );
 }
